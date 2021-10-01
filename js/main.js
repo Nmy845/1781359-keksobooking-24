283ab129c25d7ab24e1c0886bf9fa08c0c10ff2b
Nmy845 <Satuevone@gmail.com>
2021-10-01 15:42:45 +0300

    module2

---
+++ js/main.js
@@ -0,0 +1,22 @@
+
+function randomInt(from, to) {
+  if (from > to) {
+    return Math.ceil(Math.random() * (from - to) + to);
+  }
+  return Math.ceil(Math.random() * (to - from) + from);
+}
+
+
+function randomFloat(from, to, decimals) {
+  let number;
+  if (from > to) {
+    number = (Math.random() * (from - to) + to) * Math.pow(10, decimals);
+  }
+  else {
+    number = (Math.random() * (to - from) + from) * Math.pow(10, decimals);
+  }
+  return Math.ceil(number) / Math.pow(10, decimals);
+}
+
+
+
