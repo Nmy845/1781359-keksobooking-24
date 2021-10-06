37a5aa797d46e0438e85df3da27012e1aa2bf54b
Nmy845 <Satuevone@gmail.com>
2021-10-06 11:39:25 +0300

    Проверка на отрицательные числа

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 
 function randomInt(from, to) {
+  if (from < 0 || to < 0) {
+    return 'Параметры диапазона должны быть неотрицательными';
+  }
   if (from > to) {
     return Math.ceil(Math.random() * (from - to) + to);
   }
@@ -7,15 +10,19 @@ function randomInt(from, to) {
 }
 
 function randomFloat(from, to, decimals) {
+  if (from < 0 || to < 0) {
+    return 'Параметры диапазона должны быть неотрицательными';
+  }
   let number;
+  const lengthNumber = Math.ceil(decimals);
   if (from > to) {
-    number = (Math.random() * (from - to) + to) * Math.pow(10, decimals);
+    number = (Math.random() * (from - to) + to) * Math.pow(10, lengthNumber);
   }
   else {
-    number = (Math.random() * (to - from) + from) * Math.pow(10, decimals);
+    number = (Math.random() * (to - from) + from) * Math.pow(10, lengthNumber);
   }
-  return Math.ceil(number) / Math.pow(10, decimals);
+  return Math.ceil(number) / Math.pow(10, lengthNumber);
 }
 
-randomInt(1, 412);
+randomInt(1, 10);
 randomFloat(1.1, 1.2, 10);
