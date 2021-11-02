d1937129c05617f5d2972f2f235cf0d5e8cfd291
Nmy845 <Satuevone@gmail.com>
2021-11-02 13:25:17 +0300

    Module7-task2 активация и деактивация form.js

--- js/form.js
+++ js/form.js
@@ -1,3 +1,28 @@
+function activatePage(){
+  const elements = document.getElementsByClassName('ad-form');
+  const formElement = elements[0];
+  formElement.classList.remove('ad-form--disabled');
+
+  let fieldSets = formElement.getElementsByTagName('fieldset');
+  for (let i=0;i<fieldSets.length;i++) {
+	  fieldSets[i].disabled=false;
+  }
+
+  const elementsMap = document.getElementsByClassName('map__filters');
+  const mapElement = elementsMap[0];
+  mapElement.classList.remove('map__filters--disabled');
+
+  fieldSets = mapElement.getElementsByTagName('fieldset');
+  for (let i=0;i<fieldSets.length;i++) {
+	  fieldSets[i].disabled=false;
+  }
+
+  fieldSets = mapElement.getElementsByTagName('select');
+  for (let i=0;i<fieldSets.length;i++) {
+	  fieldSets[i].disabled=false;
+  }
+}
+
 function deactivatePage(){
   const elements = document.getElementsByClassName('ad-form');
   const formElement = elements[0];
@@ -23,28 +48,4 @@ function deactivatePage(){
   }
 }
 
-function activatePage(){
-  const elements = document.getElementsByClassName('ad-form');
-  const formElement = elements[0];
-  formElement.classList.remove('ad-form--disabled');
-
-  let fieldSets = formElement.getElementsByTagName('fieldset');
-  for (let i=0;i<fieldSets.length;i++) {
-    fieldSets[i].disabled=false;
-  }
-
-  const elementsMap = document.getElementsByClassName('map__filters');
-  const mapElement = elementsMap[0];
-  mapElement.classList.remove('map__filters--disabled');
-
-  fieldSets = mapElement.getElementsByTagName('fieldset');
-  for (let i=0;i<fieldSets.length;i++) {
-    fieldSets[i].disabled=false;
-  }
-
-  fieldSets = mapElement.getElementsByTagName('select');
-  for (let i=0;i<fieldSets.length;i++) {
-    fieldSets[i].disabled=false;
-  }
-}
 export {deactivatePage , activatePage};
