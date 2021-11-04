91ca5b38fc2c73f18a7ccd5166cff0cfa580b76b
Nmy845 <Satuevone@gmail.com>
2021-11-04 20:12:37 +0300

    Module 8 task 2

--- js/validation.js
+++ js/validation.js
@@ -3,28 +3,33 @@ const formType = form.querySelector('#type');
 const formPrice = form.querySelector('#price');
 const formRooms = form.querySelector('#room_number');
 const formCapacity = form.querySelector('#capacity');
+const flatPrice = 1000;
+const bungalowPrice = 0;
+const hotelPrice = 3000;
+const housePrice = 5000;
+const palacePrice = 10000;
 
 formType.addEventListener('change', (event) => {
   switch(event.target.value) {
     case 'flat':
-      formPrice.min=1000;
-      formPrice.placeholder=1000;
+      formPrice.min=flatPrice;
+      formPrice.placeholder=flatPrice;
       break;
     case 'bungalow':
-      formPrice.min=0;
-      formPrice.placeholder=0;
+      formPrice.min=bungalowPrice;
+      formPrice.placeholder=bungalowPrice;
       break;
     case 'hotel':
-      formPrice.min=3000;
-      formPrice.placeholder=3000;
+      formPrice.min=hotelPrice;
+      formPrice.placeholder=hotelPrice;
       break;
     case 'house':
-      formPrice.min=5000;
-      formPrice.placeholder=5000;
+      formPrice.min=housePrice;
+      formPrice.placeholder=housePrice;
       break;
     case 'palace':
-      formPrice.min=10000;
-      formPrice.placeholder=10000;
+      formPrice.min=palacePrice;
+      formPrice.placeholder=palacePrice;
       break;
   }
 },
@@ -43,6 +48,18 @@ formAfter.addEventListener('change', (event) => {
   formFrom.querySelector(`option[value="${formValue}"]`).selected='selected';
 });
 
+const palace = formCapacity.querySelector('option[value="0"]');
+const oneRoom = formCapacity.querySelector('option[value="3"]');
+const twoRoom = formCapacity.querySelector('option[value="2"]');
+const threeRoom =  formCapacity.querySelector('option[value="1"]');
+
+function roomValidation () {
+  oneRoom.disabled=true;
+  twoRoom.disabled=true;
+  palace.disabled=true;
+}
+roomValidation(); //фикс того,что до обработки события даёт выбрать любое количество мест. Можно сделать через поиск по Selected ,а selected по умолчанию 1 комната.
+
 formRooms.addEventListener('change', (event) => {
   const options = formCapacity.querySelectorAll('option');
   for (let i=0;i<options.length;i++){
@@ -50,21 +67,21 @@ formRooms.addEventListener('change', (event) => {
   }
   switch(event.target.value) {
     case '3':
-      formCapacity.querySelector('option[value="0"]').disabled=true;
+      palace.disabled=true;
       break;
     case '2':
-      formCapacity.querySelector('option[value="3"]').disabled=true;
-      formCapacity.querySelector('option[value="0"]').disabled=true;
+      oneRoom.disabled=true;
+      palace.disabled=true;
       break;
     case '1':
-      formCapacity.querySelector('option[value="3"]').disabled=true;
-      formCapacity.querySelector('option[value="2"]').disabled=true;
-      formCapacity.querySelector('option[value="0"]').disabled=true;
+      oneRoom.disabled=true;
+      twoRoom.disabled=true;
+      palace.disabled=true;
       break;
     case '100':
-      formCapacity.querySelector('option[value="3"]').disabled=true;
-      formCapacity.querySelector('option[value="2"]').disabled=true;
-      formCapacity.querySelector('option[value="1"]').disabled=true;
+	 	oneRoom.disabled=true;
+	 	twoRoom.disabled=true;
+	 	threeRoom.disabled=true;
       break;
   }
 });
